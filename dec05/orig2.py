471901f763c18e7cc56f0d598488504a47210499
Kyle Matuszczak <kyle.l.matuszczak@gmail.com>
2023-12-09 21:50:43 -0500

    Highly improved time by moving string manipulation to only once

--- dec05/orig2.py
+++ dec05/orig2.py
@@ -1,95 +1,39 @@
 #!/usr/bin/env python3
 
-from sys import argv
-
-class Seed_Bag:
-    def __init__(self, start, step):
-        self.start = start
-        self.step = step
-
-    def __contains__(self, item):
-        return self.start <= item < (self.start + self.step)
-
-    def __str__(self):
-        return f"[{self.start}, {self.start + self.step})"
-
-
-class Converter:
-    def __init__(self, dst, src, steps):
-        self.dst = dst
-        self.src = src
-        self.steps = steps
-
-    def convert(self, in_val):
-        if self.src <= in_val < (self.src + self.steps):
-            return in_val + (self.dst - self.src)
-        raise ValueError
-
-    def reverse(self, in_val):
-        if self.dst <= in_val < (self.dst + self.steps):
-            return in_val + (self.src - self.dst)
-        raise ValueError
-
-    def __str__(self):
-        return f"[{self.dst}, {self.dst + self.steps}) -> [{self.src}, {self.src + self.steps})"
 
 def main():
-    data = []
     with open("input.txt", "r") as f:
-        data = [line.strip() for line in f.readlines()]
+        raw_seeds, *chunks = f.read().split("\n\n")
 
-    idx = 0
-    _, seeds = data[idx].split(": ")
-    seeds = list(map(int, seeds.split()))
-    idx += 3
+    seeds = raw_seeds.split(":")[1]  # extract number portion of string
+    seeds = list(map(int, seeds.split()))  # convert string to list(ints)
+    seed_ranges = [range(seeds[idx], seeds[idx] + seeds[idx + 1]) for idx in range(0, len(seeds), 2)]
 
-    machines = []
+    chunks.reverse()
 
-    for _ in range(6):
-        machine_category = []
-        machines.append(machine_category)
-        while data[idx] != "":
-            machine_category.append(Converter(*map(int, data[idx].split())))
-            idx += 1
-        idx += 2
+    for idx, chunk in enumerate(chunks):
+        chunks[idx] = [tuple(map(int, sieve.split())) for sieve in chunk.strip().split("\n")[1:]]
 
-    humid_to_loc = []
-    machines.append(humid_to_loc)
-    for line in data[idx:]:
-        humid_to_loc.append(Converter(*map(int, line.split())))
 
-    machines.reverse()
-
-    temp = []
-    for idx in range(0, len(seeds), 2):
-        start = seeds[idx]
-        steps = seeds[idx + 1]
-        temp.append(Seed_Bag(start, steps))
-
-    seeds = temp
-
-    curr_state = int(argv[1]) * 1000000
-    end = curr_state + 1000000
-
+    loc = -1
     found = False
-    while not found and curr_state < end:
-        curr = curr_state
-        for machine_type in machines:
-            for machine in machine_type:
-                try:
-                    curr = machine.reverse(curr)
+    while not found:
+        loc += 1
+        if (loc % 1000000 == 0):
+            print(f"{loc = }")
+        seed = loc
+        for chunk in chunks:
+            for dst, src, step in chunk:
+                if dst <= seed < (dst + step):
+                    seed += src - dst
                     break
-                except ValueError:
-                    pass
 
-        for bag in seeds:
-            if curr in bag:
+        for seed_range in seed_ranges:    
+            if seed in seed_range:
                 found = True
-                print(curr_state)
                 break
 
-        curr_state += 1
-
+    print(loc)
 
 if __name__ == '__main__':
     main()
